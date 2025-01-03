51e67f297a8d7143ef50385433b5d3576817c6a0
Alex <admin@ammina.ru>
2025-01-03 23:50:17 +0300

    Memcached - настройка сервиса

--- core/classes/Addons/AbstractMemcached.php
+++ core/classes/Addons/AbstractMemcached.php
@@ -10,22 +10,22 @@ use function AmminaISP\Core\isOn;
 
 set_time_limit(60);
 
-abstract class AbstractRedis
+abstract class AbstractMemcached
 {
 	public string $xml = '';
-	public string $settingsFile = '/usr/local/mgr5/etc/amminaisp/redis.ini';
-	public string $templateFileName = '/usr/local/mgr5/etc/templates/amminaisp/redis.template';
-	public string $redisOptionsFile = '/etc/redis/redis.conf';
+	public string $settingsFile = '/usr/local/mgr5/etc/amminaisp/memcached.ini';
+	public string $templateFileName = '/usr/local/mgr5/etc/templates/amminaisp/memcached.template';
+	public string $redisOptionsFile = '/etc/memcached.conf';
 	public string $dataAppend = '';
 
-	public string $redisRun = '/var/run/redis';
-	public string $redisUser = 'redis';
+	public string $memcachedRun = '/var/run/memcached';
+	public string $memcachedUser = 'memcache';
 
 	function __construct()
 	{
-		if (!file_exists($this->redisRun)) {
-			mkdir($this->redisRun, 0755);
-			chown($this->redisRun, $this->redisUser);
+		if (!file_exists($this->memcachedRun)) {
+			mkdir($this->memcachedRun, 0755);
+			chown($this->memcachedRun, $this->memcachedUser);
 		}
 		checkDirPath($this->settingsFile);
 		$this->xml = '';
@@ -40,7 +40,7 @@ abstract class AbstractRedis
 	 */
 	public function run(): void
 	{
-		if (isset($_SERVER['PARAM_ammina_memorylimit']) && isset($_SERVER['PARAM_sok']) && $_SERVER['PARAM_sok'] == "ok") {
+		if (isset($_SERVER['PARAM_ammina_cachesize']) && isset($_SERVER['PARAM_sok']) && $_SERVER['PARAM_sok'] == "ok") {
 			$this->saveForm();
 		} else {
 			$this->openForm();
@@ -66,31 +66,31 @@ abstract class AbstractRedis
 			$data = [];
 		}
 
-		$this->dataAppend .= '<ammina_memorylimit>' . ($data['memorylimit'] ?? 128) . '</ammina_memorylimit>';
-		$this->dataAppend .= '<ammina_databases>' . ($data['databases'] ?? 16) . '</ammina_databases>';
+		$this->dataAppend .= '<ammina_cachesize>' . ($data['cachesize'] ?? 64) . '</ammina_cachesize>';
+		$this->dataAppend .= '<ammina_maxconn>' . ($data['maxconn'] ?? 1024) . '</ammina_maxconn>';
 		$this->dataAppend .= '<ammina_issocket>' . boolToFlag((bool)($data['issocket'] ?? true)) . '</ammina_issocket>';
 	}
 
 	public function saveForm(): void
 	{
-		@exec("service redis stop");
+		@exec("service memcached stop");
 		$content = [];
-		$content[] = 'memorylimit = ' . (int)$_SERVER['PARAM_ammina_memorylimit'];
-		$content[] = 'databases = ' . (int)$_SERVER['PARAM_ammina_databases'];
+		$content[] = 'cachesize = ' . (int)$_SERVER['PARAM_ammina_cachesize'];
+		$content[] = 'maxconn = ' . (int)$_SERVER['PARAM_ammina_maxconn'];
 		$content[] = 'issocket = ' . (isOn($_SERVER['PARAM_ammina_issocket']) ? 1 : 0);
 		file_put_contents($this->settingsFile, implode("\n", $content));
 		$params = [];
-		$params[] = '<ammina_memorylimit>' . (int)$_SERVER['PARAM_ammina_memorylimit'] . '</ammina_memorylimit>';
-		$params[] = '<ammina_databases>' . (int)$_SERVER['PARAM_ammina_databases'] . '</ammina_databases>';
+		$params[] = '<ammina_cachesize>' . (int)$_SERVER['PARAM_ammina_cachesize'] . '</ammina_cachesize>';
+		$params[] = '<ammina_maxconn>' . (int)$_SERVER['PARAM_ammina_maxconn'] . '</ammina_maxconn>';
 		$params[] = '<ammina_issocket>' . checkOnOff($_SERVER['PARAM_ammina_issocket']) . '</ammina_issocket>';
 		if (!empty($params)) {
 			$this->dataAppend .= implode("", $params);
 		}
 
 		$templateReplace = [
-			"AMMINA_MAXMEMORY" => (int)$_SERVER['PARAM_ammina_memorylimit'] . "mb",
-			"AMMINA_DATABASES" => (int)$_SERVER['PARAM_ammina_databases'],
-			"AMMINA_CONNECTION" => (isOn($_SERVER['PARAM_ammina_issocket']) ? "unixsocket {$this->redisRun}/redis.sock\nunixsocketperm 766\nport 0" : "bind 127.0.0.1\nport 6379"),
+			"AMMINA_CACHESIZE" => (int)$_SERVER['PARAM_ammina_cachesize'],
+			"AMMINA_MAXCONN" => (int)$_SERVER['PARAM_ammina_maxconn'],
+			"AMMINA_CONNECTION" => (isOn($_SERVER['PARAM_ammina_issocket']) ? "-s {$this->memcachedRun}/memcached.sock\n-a 766" : "-l 127.0.0.1"),
 		];
 		$template = '';
 		if (file_exists($this->templateFileName)) {
@@ -103,13 +103,13 @@ abstract class AbstractRedis
 			file_put_contents($this->redisOptionsFile, $template);
 			$this->checkMonitoring();
 			$this->checkSystemSettings();
-			@exec("service redis start");
+			@exec("service memcached start");
 		}
 	}
 
 	protected function checkMonitoring(): void
 	{
-		addJob("ammina.redis.monitoring", []);
+		addJob("ammina.memcached.monitoring", []);
 	}
 
 	protected abstract function checkSystemSettings(): void;
---
+++ debian12/classes/Addons/Memcached.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AmminaISP\Debian12\Addons;
+
+use AmminaISP\Core\Addons\AbstractMemcached;
+
+class Memcached extends AbstractMemcached
+{
+
+	protected function checkSystemSettings(): void
+	{
+		// TODO: Implement checkSystemSettings() method.
+	}
+}
